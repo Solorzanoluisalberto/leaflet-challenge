9949f6fbd6fee773e083f22c1d3f8fb8b08b54e3
Luis Solorzano <Solorzanoluis@hotmail.com>
2021-04-24 09:57:23 -0400

    range of depth

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -45,14 +45,17 @@ function createFeatures(earthquakeData) {
         console.log(long);
         console.log(lat);
         var color = get_color(depth);
-        L.circle([lat, long], {
-            fillOpacity: 0.75,
-            color: color,
-            fillColor: color,
-            // Adjust radius
-            radius: lat * 1200
-        }).bindPopup("<h1>" + mag + "</h1> <hr> <h3>City: " + place + "</h3>").addTo(myMap);
-
+        if (depth > range.lower && depth < range.upper) {
+            var color = get_color(depth); //circle color
+            //console.log("Luis cumple");
+            L.circle([lat, long], {
+                fillOpacity: 0.75,
+                color: color,
+                fillColor: color,
+                // Adjust radius
+                radius: lat * 1200
+            }).bindPopup("<h2>Magnitude: " + mag + " depth: " + depth + " </h2> <hr> <h3>City: " + place + "</h3>").addTo(myMap);
+        }
     });
 }
 
@@ -80,6 +83,57 @@ function get_color(depth) {
     return color
 }
 
+// range of depth for legend
+function getRange(Depth_select) {
+    let lower = 0;
+    let upper = 0;
+    switch (Depth_select) {
+        case "0":
+            console.log("pintar < 10")
+            //  console.log(bigNumbers)
+            lower = -100;
+            upper = 10
+            break;
+        case "1":
+            lower = 10.1;
+            upper = 30
+            console.log("pintar entre 10 y 30")
+            console.log(Depth_select)
+            break;
+        case "2":
+            lower = 30.1;
+            upper = 50
+            break;
+        case "3":
+            lower = 50.1;
+            upper = 70
+            console.log("pintar entre 50 y 70")
+            console.log(Depth_select)
+            break;
+        case "4":
+            lower = 70.1;
+            upper = 90
+            console.log("pintar entre 70 y 90")
+            console.log(Depth_select)
+            break;
+        case "5":
+            lower = 90.1;
+            upper = 500
+            console.log("pintar > 90")
+            console.log(Depth_select)
+            break;
+        default:
+            console.log("pintar todos")
+            lower = -100;
+            upper = 500
+            break;
+    }
+    console.log(`${lower} and ${upper}`)
+    return {
+        lower,
+        upper
+    };
+}
 
 console.log(new Date());
 console.log("3");
